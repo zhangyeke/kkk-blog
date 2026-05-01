2920e7b916d3eb16f554ec9b190c4d3216166088
kkk <997610780@qq.com>
2026-05-02 01:19:42 +0800

    1

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -1,18 +1,28 @@
+/*
+ * @Author: kkk 997610780@qq.com
+ * @Date: 2025-09-17 21:38:09
+ * @LastEditors: kkk 997610780@qq.com
+ * @LastEditTime: 2026-05-02 01:19:27
+ * @FilePath: \blog\app\global-error.tsx
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 'use client' // Error boundaries must be Client Components
 import PageError from "@/components/PageError";
 export default function GlobalError({
-                                        reset,
-                                    }: {
+    reset,
+    error,
+}: {
     error: Error & { digest?: string }
     reset: () => void
 }) {
+    console.error("发生了什么错误", error, reset);
 
     return (
         // 必须定义html结构 已替换根布局
         <html>
-        <body>
-        <PageError/>
-        </body>
+            <body>
+                <PageError />
+            </body>
         </html>
     )
 }
