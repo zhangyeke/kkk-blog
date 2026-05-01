40aef55d5bce21db72b9899e80cab1e519196d12
kkk <997610780@qq.com>
2026-05-02 01:34:37 +0800

    123

--- app/global-error.tsx
+++ app/global-error.tsx
@@ -2,7 +2,7 @@
  * @Author: kkk 997610780@qq.com
  * @Date: 2025-09-17 21:38:09
  * @LastEditors: kkk 997610780@qq.com
- * @LastEditTime: 2026-05-02 01:24:23
+ * @LastEditTime: 2026-05-02 01:34:16
  * @FilePath: \blog\app\global-error.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -11,6 +11,7 @@ import PageError from "@/components/PageError";
 export default function GlobalError({
     reset,
     error,
+
 }: {
     error: Error & { digest?: string }
     reset: () => void
@@ -21,9 +22,11 @@ export default function GlobalError({
         // 必须定义html结构 已替换根布局
         <html>
             <body>
-                {JSON.stringify(error)}
 
-
+                {error.digest}
+                <div>
+                    信息：{error.message} 名称：{error.name} 堆栈：{error.stack}
+                </div>
                 {/* <PageError /> */}
             </body>
         </html>
