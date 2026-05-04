c382e74eb3b174211d34f24a768937082c3fc5dd
kkk <997610780@qq.com>
2026-05-04 16:32:12 +0800

    全局樱花组件调整

--- components/SakuraOverlay.jsx
+++ components/SakuraOverlay.jsx
@@ -2,13 +2,14 @@
  * @Author: kkk 997610780@qq.com
  * @Date: 2026-04-29 17:01:05
  * @LastEditors: kkk 997610780@qq.com
- * @LastEditTime: 2026-05-03 18:35:50
+ * @LastEditTime: 2026-05-04 16:31:43
  * @FilePath: \blog\components\SakuraOverlay.tsx
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 "use client"
 
 import { useEffect } from "react"
+import { merge } from "lodash"
 import Sakura from "@/lib/sakura"
 import "@/styles/sakura.css"
 
@@ -28,36 +29,15 @@ import "@/styles/sakura.css"
  * 使用动态 import，避免 Turbopack/Webpack 对 sakura.js 顶层 default + `new` 的错误压缩形态（i.default is not a constructor）。
  * @param {SakuraOverlayProps} props
  */
-export default function SakuraOverlay(props) {
-  const {
-    wrapperClassName = "",
-    petalClassName,
-    fallSpeed,
-    maxSize,
-    minSize,
-    delay,
-    colors,
-  } = props
-  const colorsJson = JSON.stringify(colors ?? null)
-
+export default function SakuraOverlay({ className = "", ...props }) {
   useEffect(() => {
-    console.log(`Sakura`, Sakura)
-
-    const opts = {}
-    if (petalClassName != null) opts.className = petalClassName
-    if (fallSpeed != null) opts.fallSpeed = fallSpeed
-    if (maxSize != null) opts.maxSize = maxSize
-    if (minSize != null) opts.minSize = minSize
-    if (delay != null) opts.delay = delay
-    if (colors != null) opts.colors = colors
-
+    const opts = merge({}, props)
     let instance = null
     let cancelled = false
 
     void (async () => {
       try {
         instance = new Sakura(".blog-layout", opts)
-        console.log(`instance`, instance);
         if (cancelled) {
           instance.stop(true)
           instance = null
@@ -72,12 +52,7 @@ export default function SakuraOverlay(props) {
       instance?.stop(true)
       instance = null
     }
-  }, [petalClassName, fallSpeed, maxSize, minSize, delay, colorsJson])
+  }, [props])
 
-  return (
-    <div
-      className={`pointer-events-none fixed inset-0 z-60 overflow-x-hidden ${wrapperClassName}`.trim()}
-      aria-hidden
-    />
-  )
+  return <div className={`pointer-events-none fixed inset-0 z-60 overflow-x-hidden ${className}`.trim()} aria-hidden />
 }
